f8380cb85f232f539d6eaefbe64e0781c51309f7
Kirtan Patel <kirtan@Kirtans-MacBook-Pro-4.local>
2024-01-30 22:42:21 -0500

    worked on sliding window

---
+++ 001-Arrays_Strings/max_avg_substring1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        ans = curr = sum(nums[:k])
+        for i in range(k,len(nums)):
+            curr += nums[i] - nums[i-k]
+            ans = max(ans,curr)
+        return ans/k
